a41ac7c62879e4d8ff87c644d607926c8772517f
achille56 <leclercqjm59@gmail.com>
2022-11-22 15:34:45 +0100

    exo 71

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,19 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerText = "Ce div a été ajouté en utilisant javascript";
 
 document.getElementById('viewport').appendChild(monDiv);
+
+let secondDiv = document.createElement("secondDiv")
+    secondDiv.innerText = "coucou";
+    secondDiv.style.backgroundColor = "blue";
+    secondDiv.style.border ="1px solid black";
+    secondDiv.style.width = "100px";
+    secondDiv.style.height = "100px";
+
+    document.getElementById("viewport").appendChild(secondDiv);
+
+let aSupprimer = document.getElementById("aSupprimer");
+    aSupprimer.remove();
 
 
 
